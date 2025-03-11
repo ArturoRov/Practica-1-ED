2ceac3d96f23c31c26deec63a22c0e4d2b8ad933
ArturoRov <artrvbz@ciencias.unam.mx>
2025-03-10 22:09:32 -0600

    Delete esDecendente.hs

--- esDecendente.hs
+++
@@ -1,9 +0,0 @@
-esDecendente :: Int -> Int -> Int -> Int -> Bool
-esDecendente x y z w =
-    if x > y
-        then if y > z 
-            then if z > w
-                then True
-                else False
-        else False
-    else False
