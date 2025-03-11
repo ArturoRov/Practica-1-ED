4d753f171aaff71cef5c4add02ce66852329a313
ArturoRov <artrvbz@ciencias.unam.mx>
2025-03-10 22:08:01 -0600

    Delete areaTriangulo.hs

--- areaTriangulo.hs
+++
@@ -1,6 +0,0 @@
-areaTriangulo :: Float -> Float -> Float -> Float
-areaTriangulo x y z = 
-    sqrt(((x + y + z) / 2)*
-    (((x + y + z) / 2)-x)*
-    (((x + y + z) / 2)-y)*
-    (((x + y + z) / 2)-z)) 
