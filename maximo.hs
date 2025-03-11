f0d8ea7e28687b0a574c1f04834087ce4233606b
ArturoRov <artrvbz@ciencias.unam.mx>
2025-03-10 22:10:01 -0600

    Delete maximo.hs

--- maximo.hs
+++
@@ -1,9 +0,0 @@
-maximo :: Int -> Int -> Int -> Int
-maximo x y z =
-    if x > y
-        then if x > z
-            then x
-            else z
-        else if y > z
-            then y
-            else z
